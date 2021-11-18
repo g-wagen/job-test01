dbc0bc3043e92da16f472691dc34fed1b2094352
g-wagen <>
2021-11-18 20:03:43 +0100

    prettier formatting

--- 01-sql_view.sql
+++ 01-sql_view.sql
@@ -1,6 +1,11 @@
-CREATE VIEW v_publisher 
-AS SELECT 
-	CAST(id AS INT) AS id, 
-	CAST(sector_id AS INT) AS sector_id, 
-	partition_market 
-FROM publisher
+-- Create the view please
+CREATE VIEW v_publisher
+AS
+SELECT
+    id,
+    -- Remove decimal point by converting the column to integer
+	CAST(sector_id AS INT) AS sector_id,
+    partition_market
+FROM
+    publisher
+
