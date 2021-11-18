15788c364d888b4f71730d2c270b962487be3512
g-wagen <79096832+g-wagen@users.noreply.github.com>
2021-11-18 02:26:07 +0100

    calculated the click through rate by sector (#4)
    Co-authored-by: g-wagen <>

---
+++ 02-click-through-rate.sql
@@ -0,0 +1,21 @@
+SELECT 
+--	Grab columns from different tables
+	ci.publisher_id,
+	vp.sector_id,
+	s.name AS sector_name,
+--	
+--	Sum up all the clicks and impressions
+	SUM(ci.clicks) AS clicks,
+	SUM(ci.reached_user) AS reached_user, -- Unsure what this number represents
+	SUM(ci.impressions) AS impressions,
+--  	
+--  Calculate Click-Through-Rate
+	ROUND(SUM(ci.clicks)/SUM(ci.impressions) * 100, 2) AS CTR
+FROM agg_clicks_impressions ci
+--
+--  Join v_publisher and sector to agg_clicks_impressions
+LEFT JOIN v_publisher vp ON ci.publisher_id = vp.id
+LEFT JOIN sector s ON vp.sector_id = s.sector_id
+--
+--  Finally group the output by the sector
+GROUP BY vp.sector_id
