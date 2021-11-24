f51117bc27260c7ac8edda2d0116b980e4c7b300
g-wagen <79096832+g-wagen@users.noreply.github.com>
2021-11-24 13:39:37 +0100

    corrected the sql query for the last question and ctr calculation (#12)
    Co-authored-by: g-wagen <>

--- Scripts/03b-brochures-activity-dec2020.sql
+++ Scripts/03b-brochures-activity-dec2020.sql
@@ -1,17 +1,25 @@
 SELECT
-	vp.sector_id,
-	c.brochure_id,
-	c.pfrom,
-	c.puntil,
-	COUNT(vp.sector_id) AS active
+    vp.sector_id,
+    s.name,
+    c.brochure_id,
+    c.pfrom,
+    c.puntil,
+    COUNT(DISTINCT c.title) AS active
 FROM
-	content c
-	-- Grab our trusty v_publisher view to access publisher ids and sectors
+    content c
+    -- Grab our trusty v_publisher view
+    -- to access publisher ids and sectors
+LEFT JOIN agg_clicks_impressions ci ON
+    ci.brochure_id = c.brochure_id
 LEFT JOIN v_publisher vp ON
-	c.publisher_id = vp.id
+    c.publisher_id = vp.id
+LEFT JOIN sector s ON
+    s.sector_id = vp.sector_id
 WHERE
-	-- Only brochures that appeared in December 2020
-	c.pfrom BETWEEN '2020-12-01' AND '2020-12-31'
+    -- Only brochures that appeared in December 2020
+    '2020-12-31' BETWEEN c.pfrom AND c.puntil
 GROUP BY
-	-- Group by sector to count the active brochures
-	vp.sector_id
+    -- Group by sector to count the active brochures
+    vp.sector_id
+ORDER BY active DESC
+
